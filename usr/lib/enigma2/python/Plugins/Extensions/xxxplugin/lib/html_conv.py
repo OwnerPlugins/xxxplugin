479e60f6aa8a0c7c5f6cda0255528cdb783bf606
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2026-04-18 22:52:04 +0200

    Update html_conv.py

--- usr/lib/enigma2/python/Plugins/Extensions/xxxplugin/lib/html_conv.py
+++ usr/lib/enigma2/python/Plugins/Extensions/xxxplugin/lib/html_conv.py
@@ -1,94 +1,87 @@
+import re
+import sys
+import six
+import types
 from six import unichr, iteritems
 from six.moves import html_entities
-import sys
-import types
-import re
-PY2 = sys.version_info[0] == 2
-PY3 = sys.version_info[0] == 3
 
-if PY3:
-	string_types = (str,)
-	integer_types = (int,)
-	class_types = (type,)
-	text_type = str
-	binary_type = bytes
+try:
+    unicode
+except NameError:
+    unicode = str  # Python 3 fallback
 
-	MAXSIZE = sys.maxsize
+try:
+    basestring
+except NameError:
+    basestring = str  # fake definition for linter
+
+try:
+    long
+except NameError:
+    long = int  # fake definition for linter
+
+
+if sys.version_info[0] < 3:
+    string_types = (basestring,)
+    integer_types = (int, long)
+    text_type = unicode
 else:
-	string_types = (str,)
-	integer_types = (int,)
-	class_types = (type, types.ClassType)
-	text_type = str
-	binary_type = str
+    string_types = (str,)
+    integer_types = (int,)
+    text_type = str
 
-	if sys.platform.startswith("java"):
-		# Jython always uses 32 bits.
-		MAXSIZE = int((1 << 31) - 1)
-	else:
-		# It's possible to have sizeof(long) != sizeof(Py_ssize_t).
-		class X(object):
 
-			def __len__(self):
-				return 1 << 31
-		try:
-			len(X())
-		except OverflowError:
-			# 32-bit
-			MAXSIZE = int((1 << 31) - 1)
-		else:
-			# 64-bit
-			MAXSIZE = int((1 << 63) - 1)
-		del X
+class_types = (type,) if six.PY3 else (type, types.ClassType)
+# text_type = six.text_type  # unicode in Py2, str in Py3
+binary_type = six.binary_type  # str in Py2, bytes in Py3
+MAXSIZE = sys.maxsize  # Compatibile con entrambe le versioni
 
-_UNICODE_MAP = {k: chr(v) if PY3 else unichr(v) for k, v in iteritems(html_entities.name2codepoint)}
+_UNICODE_MAP = {
+    k: unichr(v) for k,
+    v in iteritems(
+        html_entities.name2codepoint)}
 _ESCAPE_RE = re.compile("[&<>\"']")
-_UNESCAPE_RE = re.compile(r"&\s*(#?)(\w+?)\s*;")  # Whitespace handling added due to "hand-assed" parsers of html pages
-# Dictionary for escaping special HTML characters
+_UNESCAPE_RE = re.compile(r"&\s*(#?)(\w+?)\s*;")
 _ESCAPE_DICT = {
-	"&": "&amp;",
-	"<": "&lt;",
-	">": "&gt;",
-	'"': "&quot;",
-	"'": "&apos;",
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;",
 }
 
 
-def ensure_str(s, encoding='utf-8', errors='strict'):
-	"""Coerce *s* to `str`.
+def ensure_str(s, encoding="utf-8", errors="strict"):
+    """Coerce *s* to `str`.
 
-	For Python 2:
-	  - `unicode` -> encoded to `str`
-	  - `str` -> `str`
-
-	For Python 3:
-	  - `str` -> `str`
-	  - `bytes` -> decoded to `str`
-	"""
-	# Optimization: Fast return for the common case.
-	if type(s) is str:
-		return s
-	if PY2 and isinstance(s, text_type):
-		return s.encode(encoding, errors)
-	elif PY3 and isinstance(s, binary_type):
-		return s.decode(encoding, errors)
-	elif not isinstance(s, (text_type, binary_type)):
-		raise TypeError("not expecting type '%s'" % type(s))
-	return s
+    - In Python 2:
+      - `unicode` -> encoded to `str`
+      - `str` -> `str`
+    - In Python 3:
+      - `str` -> `str`
+      - `bytes` -> decoded to `str`
+    """
+    if isinstance(s, str):
+        return s
+    if isinstance(s, binary_type):
+        return s.decode(encoding, errors)
+    raise TypeError("not expecting type '%s'" % type(s))
 
 
 def html_escape(value):
-    value = ensure_str(value).strip()
-    return _ESCAPE_RE.sub(lambda match: _ESCAPE_DICT.get(match.group(0), match.group(0)), value)
+    return _ESCAPE_RE.sub(
+        lambda match: _ESCAPE_DICT[match.group(0)], ensure_str(value).strip())
 
 
 def html_unescape(value):
-	return _UNESCAPE_RE.sub(_convert_entity, ensure_str(value).strip())
+    return _UNESCAPE_RE.sub(_convert_entity, ensure_str(value).strip())
 
 
 def _convert_entity(m):
-	if m.group(1) == "#":
-		try:
-			return chr(int(m.group(2)[1:], 16)) if m.group(2)[:1].lower() == "x" else chr(int(m.group(2)))
-		except ValueError:
-			return "&#%s;" % m.group(2)
-	return _UNICODE_MAP.get(m.group(2), "&%s;" % m.group(2))
+    if m.group(1) == "#":
+        try:
+            return unichr(int(m.group(2)[1:], 16)) if m.group(
+                2)[:1].lower() == "x" else unichr(int(m.group(2)))
+        except ValueError:
+            return "&#%s;" % m.group(2)
+    return _UNICODE_MAP.get(m.group(2), "&%s;" % m.group(2))
